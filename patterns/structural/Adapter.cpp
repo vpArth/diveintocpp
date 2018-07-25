8768c620a3adc92a71a4f1dd67133f2682997964
Alexander Deider (home) <arth.inbox@gmail.com>
2018-07-25 23:01:32 +0700

    patterns: Adapter via private inheritance

--- patterns/structural/Adapter.cpp
+++ patterns/structural/Adapter.cpp
@@ -2,11 +2,15 @@
 
 class FahrenheitSensor // Existing class
 {
+  float t = 132.0;
 public:
   float getFahrenheitTemp() {
-    float t = 132.0;
-    // Some code
+    // Some functionality
     return t;
+  }
+protected:
+  void adjust() {
+    t += 0.1;
   }
 };
 
@@ -14,6 +18,7 @@ class Sensor {
 public:
   virtual ~Sensor() = default;
   virtual float getTemperature() = 0;
+  virtual void adjust() = 0;
 };
 
 class Adapter : public Sensor {
@@ -26,13 +31,28 @@ public:
   float getTemperature() override {
     return float((p_fsensor->getFahrenheitTemp() - 32.0) * 5.0 / 9.0);
   }
+  void adjust() override {} // can't use protected one
 private:
   FahrenheitSensor *p_fsensor;
 };
 
+class InhAdapter : public Sensor, private FahrenheitSensor {
+public:
+  InhAdapter() = default;
+  float getTemperature() override {
+    return static_cast<float>((getFahrenheitTemp() - 32.0) * 5.0 / 9.0);
+  }
+  void adjust() override {
+    FahrenheitSensor::adjust();
+  }
+};
+
 int main() {
   Sensor *p = new Adapter(new FahrenheitSensor);
-  std::cout << "Celsius temperature = " << p->getTemperature() << std::endl;
+  Sensor *ip = new InhAdapter();
+  ip->adjust();
+  std::cout << "T = " << p->getTemperature() << std::endl;
+  std::cout << "T = " << ip->getTemperature() << std::endl;
   delete p;
   return 0;
 }
